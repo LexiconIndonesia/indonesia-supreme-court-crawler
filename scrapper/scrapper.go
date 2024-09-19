668ac63f7af5edcfe5e58f8880113170aaaa9aeb
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-09-19 20:45:02 +0700

    Bugfix/adryan set to non concurrent scrapper (#5)
    * fix: disable parallelism because of race condition
    
    so, the url in extraction can be different from the url_frontier because
    of race condition, in the end we will disable parallelism so the data
    can be consistent
    
    * fix: remove thread

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -29,7 +29,8 @@ func StartScraper() {
 
 	log.Info().Msg("Unscrapped URLs: " + strconv.Itoa(len(list)))
 
-	q, err := queue.New(2, &queue.InMemoryQueueStorage{MaxSize: 10000})
+	q, err := queue.New(1, &queue.InMemoryQueueStorage{MaxSize: 100000})
+
 	if err != nil {
 		log.Error().Err(err).Msg("Error creating queue")
 	}
